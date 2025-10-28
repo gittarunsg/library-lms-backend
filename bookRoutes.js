07f728d1f28264b40f6631b96067b5ee0e4dc66b
Tarun S <23f2002939@ds.study.iitm.ac.in>
2025-10-28 12:04:14 +0530

    Add files via upload

---
+++ bookRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import Book from "../models/Book.js";
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const books = await Book.find();
+  res.json(books);
+});
+
+router.post("/", async (req, res) => {
+  const newBook = new Book(req.body);
+  await newBook.save();
+  res.json({ message: "Book added successfully" });
+});
+
+export default router;
